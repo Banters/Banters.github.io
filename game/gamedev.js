d0a3383727e8e0fa6f3a68c2d3a950cfd5e33152
Banters <210201@hkis.edu.hk>
2016-06-08 09:37:39 +0800

    Added into website/game folder
    FINAL COMMIT

---
+++ game/gamedev.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+
+<<<<<<< HEAD
+$("Enemy").click(function(){
+	$("counter").show(200);
+});
+//Your jquery goes here
+=======
+$("one").animate();
+$("Timer").setTimeout("alert('Boom!');", 2000);$("p").click(function(){
+  // action goes here!!
+});
+<<<<<<< HEAD
+>>>>>>> origin/master
+
+=======
+>>>>>>> origin/master
+});
